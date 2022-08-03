6f803c83414bc9d705791a34afed9637fff4bc28
Michel Silva <michelalmeidasilva@outlook.pt>
2022-08-03 11:55:19 -0300

    wip: added a config to jest

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['/node_modules/(?!(@react-native|react-native)/).*/']
+};
